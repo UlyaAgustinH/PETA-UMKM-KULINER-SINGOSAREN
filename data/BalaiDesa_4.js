761e2c301fada7281767fd12982c3e4cf0be1a67
UlyaAgustinH <118214169+UlyaAgustinH@users.noreply.github.com>
2023-07-14 15:35:25 +0700

    Add files via upload

---
+++ data/BalaiDesa_4.js
@@ -0,0 +1,1 @@
+var json_BalaiDesa_4 = {"type":"FeatureCollection","name":"BalaiDesa_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","Tempat":null},"geometry":{"type":"Point","coordinates":[110.400425471599078,-7.834598502814639]}}]}
